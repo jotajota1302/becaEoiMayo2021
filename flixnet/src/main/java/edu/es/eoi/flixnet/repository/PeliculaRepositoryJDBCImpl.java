ca2764e5a3f16844375fa12b8382085bba4e1a9a
jj <jotajota1302@hotmail.com>
2021-06-30 17:36:33 +0200

    add junit4 to maven

--- flixnet/src/main/java/edu/es/eoi/flixnet/repository/PeliculaRepositoryJDBCImpl.java
+++ flixnet/src/main/java/edu/es/eoi/flixnet/repository/PeliculaRepositoryJDBCImpl.java
@@ -7,7 +7,6 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Properties;
 
--- flixnet/src/test/java/edu/es/eoi/flixnet/PeliculaRepositoryJDBCImplTest.java
+++ flixnet/src/test/java/edu/es/eoi/flixnet/PeliculaRepositoryJDBCImplTest.java
@@ -1,24 +1,27 @@
 package edu.es.eoi.flixnet;
 
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
 import edu.es.eoi.flixnet.entities.GeneroEnum;
 import edu.es.eoi.flixnet.repository.PeliculaRepository;
 import edu.es.eoi.flixnet.repository.PeliculaRepositoryJDBCImpl;
 
-class PeliculaRepositoryJDBCImplTest {
+
+public class PeliculaRepositoryJDBCImplTest {
 
 	@Test
-	void testBuscarPorGenero() {
+	public void testBuscarPorGenero() {
 		
 		PeliculaRepository repository= new PeliculaRepositoryJDBCImpl();
 		
 		try {
 		
 		assertNotNull(repository.buscarPorGenero(GeneroEnum.ACCION).size());	
+		assertEquals(0,repository.buscarPorGenero(GeneroEnum.FANTASIA).size());
 		
 		} catch (Exception e) {		
 			e.printStackTrace();
