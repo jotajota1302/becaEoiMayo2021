da811278e0df31fe4a431df8951f27eb6a098d23
jj <jotajota1302@hotmail.com>
2021-07-05 19:08:42 +0200

    commit

---
+++ myfirstweb/src/main/java/edu/es/eoi/MyServlet.java
@@ -0,0 +1,25 @@
+package edu.es.eoi;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MyServlet extends HttpServlet{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	
+		resp.getOutputStream().print("Bienvenido a mi app");
+		
+	}
+	
+	
+
+}
