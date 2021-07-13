eb94a602a32e8a32d9a948424399924692ed0332
jj <jotajota1302@hotmail.com>
2021-07-13 19:14:56 +0200

    commit

--- JPAExample/src/main/java/edu/es/eoi/controllers/ClienteController.java
+++ JPAExample/src/main/java/edu/es/eoi/controllers/ClienteController.java
@@ -7,9 +7,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import edu.es.eoi.dtos.ClienteDto;
@@ -36,43 +36,20 @@ public class ClienteController {
 	}
 
 	@RequestMapping(method = RequestMethod.POST, value = "/clientes")
-	public ResponseEntity<?> createCliente(@RequestParam String dni, @RequestParam String nombre,
-			@RequestParam String direccion) {
+	public ResponseEntity<ClienteDto> createCliente(@RequestBody ClienteDto cliente) {
 
-		if(null==dni||dni.isEmpty()) {			
-			return new ResponseEntity<String>(HttpStatus.BAD_REQUEST);		
-		}
-		
-		if (service.findCliente(dni) != null) {
+			service.saveCliente(cliente);
 
-			return new ResponseEntity<String>(HttpStatus.NOT_ACCEPTABLE);
-			
-		} else {
-			
-			service.saveCliente(ClienteDto.builder().dni(dni).nombre(nombre).direccion(direccion).build());
-
-			return new ResponseEntity<String>(HttpStatus.CREATED);
-		}
+			return new ResponseEntity<ClienteDto>(HttpStatus.CREATED);		
 
 	}
 	
 	@RequestMapping(method = RequestMethod.PUT, value = "/clientes/{dni}")
-	public ResponseEntity<?> updateCliente(@PathVariable String dni, @RequestParam String nombre,@RequestParam String direccion) {
+	public ResponseEntity<?> updateCliente(@PathVariable String dni, @RequestBody ClienteDto cliente) {
+					
+			service.updateCliente(cliente);
 
-		if(null==dni||dni.isEmpty()) {			
-			return new ResponseEntity<String>(HttpStatus.BAD_REQUEST);		
-		}
-		
-		if (service.findCliente(dni) == null) {
-
-			return new ResponseEntity<String>(HttpStatus.NOT_ACCEPTABLE);
-			
-		} else {
-			
-			service.updateCliente(ClienteDto.builder().dni(dni).nombre(nombre).direccion(direccion).build());
-
-			return new ResponseEntity<String>(HttpStatus.CREATED);
-		}
+			return new ResponseEntity<String>(HttpStatus.CREATED);		
 
 	}
 	
