5d218c325ecca3a2129cf77a8d3237253c410e80
jj <jotajota1302@hotmail.com>
2021-07-13 19:17:07 +0200

    coomit

--- JPAExample/src/main/java/edu/es/eoi/controllers/ClienteController.java
+++ JPAExample/src/main/java/edu/es/eoi/controllers/ClienteController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -35,7 +36,7 @@ public class ClienteController {
 
 	}
 
-	@RequestMapping(method = RequestMethod.POST, value = "/clientes")
+	@PostMapping("/clientes")
 	public ResponseEntity<ClienteDto> createCliente(@RequestBody ClienteDto cliente) {
 
 			service.saveCliente(cliente);
